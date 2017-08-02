ec7b89d271ddc4c7bac46c8cb9f6e453bf6616c6
Chris-Dav <christopher.davies02@gmail.com>
2017-08-02 00:37:49 -0700

    moved links to app.erb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
 
-  get 'contact-us' => 'pages#contact-us'
+  get 'contact' => 'pages#contact'
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
